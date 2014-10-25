1eddeb32247c8ccea7aa152775d76d5f89d3777d
Robert Garza <chorejas@Robertos-MacBook-Pro.local>
2014-10-25 00:39:26 -0500

    version bump

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'none'
 license          ''
 description      'Installs/Configures icanhazdevops'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'apache2'
 depends 'iptables'
